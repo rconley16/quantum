2f4b7d5b1e6cd22dd7e8fc51e28a512b21f0c28f
Rebecca Conley <conley.rebecca@gmail.com>
2021-05-19 13:45:44 -0400

    checking lowest energy

--- two_by_two/check_lowest_E.py
+++ two_by_two/check_lowest_E.py
@@ -6,6 +6,9 @@ sampler_auto = EmbeddingComposite(DWaveSampler(solver={'qpu': True}))
 # Set Q for the problem QUBO
 # c for coefficient
 
+
+
+
 linear = {('q11','q11'):26, ('q12','q12'):72 , ('q13','q13'):-6, ('q14','q14'):8, ('q21','q21'):-13, ('q22','q22'):-16, ('q23','q23'):23, ('q24','q24'):56}
 
 
@@ -16,10 +19,17 @@ Q.update(quadratic)
 
 
 
-# Minor Embed and sample 1000 times on a default D-Wave sytem
+# Minor Embed and sample 10 times on a default D-Wave sytem, repeat up to 100 times
+#   or until the lowest energy is found
 tic = time.perf_counter()
-sampleset = sampler_auto.sample_qubo(Q, num_reads=1000)
-print(sampleset)
+for i in range(1,100):
+    sampleset = sampler_auto.sample_qubo(Q, num_reads=10)
+    print(sampleset)
+    print(sampleset.first[1])
+    if sampleset.first[1] == -26:
+        print("Lowest energy was reached. \n")
+        print("This took {0} samples of 10".format(i))
+        break
 toc = time.perf_counter()
 print(f"Found the lowest energy in {toc - tic:0.10f} seconds")
 print(sampleset.first)
